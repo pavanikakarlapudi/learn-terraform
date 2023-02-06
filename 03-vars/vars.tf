b30532884454cd2a8fd2e26f7edb85b02611b877
Raghu K <raghuk.vit@gmail.com>
2023-02-06 07:16:38 +0530

    Update learn-terraform/

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -61,3 +61,12 @@ variable "demo1" {}
 output "demo1" {
   value = var.demo1
 }
+
+## Variables from SHELL env variables
+variable "demo2" {
+  default = null
+}
+
+output "demo2" {
+  value = var.demo2
+}
