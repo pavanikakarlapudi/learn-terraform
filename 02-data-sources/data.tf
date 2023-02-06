a5840f8c6db41fc074d47d0817ca31b486f2abbf
Raghu K <raghuk.vit@gmail.com>
2023-02-06 07:03:22 +0530

    Update learn-terraform/

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -1,20 +1,24 @@
-data "aws_ami" "example" {
+data "aws_ami" "centos8" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami.example
+  value = data.aws_ami.centos8
 }
 
+
+
+
+output "public_ip" {
+  value = data.aws_instance.foo.public_ip
+}
+
+output "private_ip" {
+  value = data.aws_instance.foo.private_ip
+}
 
 data "aws_instance" "foo" {
   instance_id = "i-0d5fc7fb99c768588"
 }
-
-
-output "instance" {
-  value = data.aws_instance.foo.public_ip
-}
-
