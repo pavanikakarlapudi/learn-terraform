21ee32c4a6393a6d15af5b24a7514e62edd526e6
Raghu K <raghuk.vit@gmail.com>
2023-02-07 06:52:45 +0530

    Update learn-terraform/

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -9,8 +9,9 @@ data "aws_ami" "centos8" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "test-centos8"
@@ -27,3 +28,27 @@ terraform {
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
