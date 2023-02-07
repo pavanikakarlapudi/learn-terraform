a0ab9d9c125070ee928f0952a053a5b220b98f9e
Raghu K <raghuk.vit@gmail.com>
2023-02-07 07:24:50 +0530

    Update learn-terraform/

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -14,5 +14,6 @@ data "aws_ami" "centos8" {
 }
 
 output "instance_profile" {
-  value = try(aws_instance.web.*.id[1], "")
+  value = aws_instance.web.*.id[1]
+  #value = try(aws_instance.web.*.id[1], "")
 }
