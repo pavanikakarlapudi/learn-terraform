048f0847027fa1a1a90d0803541b1fc361927624
Raghu K <raghuk.vit@gmail.com>
2023-02-08 07:00:24 +0530

    Update learn-terraform/

--- 08-loops/for-each/main.tf
+++ 08-loops/for-each/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   for_each      = var.components
   ami           = data.aws_ami.centos8.id
-  instance_type = each.type.instance_type
+  instance_type = each.value.instance_type
 
   tags = {
     Name = each.value["name"]
